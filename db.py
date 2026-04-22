384a97281b08f76db7e6e9888ced8f71a2826fba
LuisVilla12 <luisji2312ia@gmail.com>
2026-04-22 17:38:03 -0600

    Reetructuracion

--- db.py
+++ db.py
@@ -2,13 +2,15 @@ import os
 import mysql.connector
 
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST', 'database'),
+    'host': os.getenv('DB_HOST', 'localhost'),
     'user': os.getenv('DB_USER', 'root'),
     'password': os.getenv('DB_PASSWORD', 'lkqaz923'),
     'database': os.getenv('DB_NAME', 'easycountcol'),
-    'port': int(os.getenv('DB_PORT', '3306')),
+    'port': int(os.getenv('DB_PORT', '3307')),
     'charset': 'utf8mb4',
     'collation': 'utf8mb4_general_ci'
 }
 def get_db():
-    return mysql.connector.connect(**DB_CONFIG)
+    db=mysql.connector.connect(**DB_CONFIG)
+    print("Conexión a la base de datos exitosa")
+    return db
